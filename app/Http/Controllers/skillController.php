7494dadd02189899410bb7a11c015a5e12449c55
LIANGYU <n795113@gmail.com>
2021-10-13 22:55:57 +0800

    feat: print out comparing schedule

--- app/Http/Controllers/skillController.php
+++ app/Http/Controllers/skillController.php
@@ -4,7 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class skillController extends Controller
+class SkillController extends Controller
 {
-    //
+    private const SKILLS = [
+        'planning',
+        'imaginate',
+        'teach',
+        'console',
+        'research',
+    ];
+
+    public function index()
+    {
+        dd(self::SKILLS);
+    }
+
+    public function compare()
+    {
+        $list = self::SKILLS;
+        $len = count($list);
+        $rounds = $len - 1;
+        $gameSchedule = [];
+
+        // 車輪戰
+        for ($i = 0; $i < $len - 1; $i++) {
+            for ($j = 0; $j < $rounds; $j++) {
+                printf(
+                    "%s:%s vs %s:%s</br>",
+                    $i,
+                    $list[$i],
+                    $rounds  - $j,
+                    $list[$rounds  - $j]
+                );
+                $gameSchedule[]= [$i, $rounds  - $j];
+            }
+            $rounds--;
+        }
+        dd($gameSchedule);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SkillController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('skills', SkillController::class)->only([
+    'index'
+]);
+Route::get('skills/compare', [SkillController::class, 'compare']);
