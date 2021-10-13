ed6ab91b8118fa46eb626187a32e3972a1f5fdaa
LIANGYU <n795113@gmail.com>
2021-10-14 00:22:50 +0800

    play with redis list

--- app/Http/Controllers/skillController.php
+++ app/Http/Controllers/skillController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class SkillController extends Controller
 {
@@ -16,12 +17,22 @@ class SkillController extends Controller
 
     public function index()
     {
-        dd(self::SKILLS);
+        $len = Redis::llen('skills');
+        
+        // $list = Redis::get('skills.0');
+        // Redis::del('skills');
+        $task = Redis::lpop('skills');
+        var_dump('handle '.$task."\n");
+
+        $list = Redis::lrange('skills', 0, $len-1);
+        dd('rest tasks:', $list);
+
     }
 
     public function compare()
     {
         $list = self::SKILLS;
+        // Redis::set('skills', $list);
         $len = count($list);
         $rounds = $len - 1;
         $gameSchedule = [];
@@ -29,14 +40,15 @@ class SkillController extends Controller
         // 車輪戰
         for ($i = 0; $i < $len - 1; $i++) {
             for ($j = 0; $j < $rounds; $j++) {
+                Redis::lpush('skills', $list[$i] . ' vs. ' . $list[$rounds  - $j]);
                 printf(
                     "%s:%s vs %s:%s</br>",
                     $i,
                     $list[$i],
                     $rounds  - $j,
-                    $list[$rounds  - $j]
+                    $list[$rounds - $j]
                 );
-                $gameSchedule[]= [$i, $rounds  - $j];
+                $gameSchedule[]= [$i, $rounds - $j];
             }
             $rounds--;
         }
